ddfeb0c5a3189ce2fff2ddfdbfa2b130e2e8d3fb
samalouty <128055267+samalouty@users.noreply.github.com>
2023-05-19 18:43:00 +0300

    Viewer 15x15 cells
    15x15 cells

---
+++ Last of Us - Legacy/src/views/Main.java
@@ -0,0 +1,45 @@
+package views;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private static final int GRID_SIZE = 15;
+    private static final int CELL_SIZE = 30;
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        BorderPane root = new BorderPane();
+        GridPane gridPane = new GridPane();
+
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                Rectangle rectangle = new Rectangle(CELL_SIZE, CELL_SIZE);
+                rectangle.setFill(Color.WHITE);
+                rectangle.setStroke(Color.BLACK);
+
+                GridPane.setRowIndex(rectangle, row);
+                GridPane.setColumnIndex(rectangle, col);
+                gridPane.getChildren().add(rectangle);
+            }
+        }
+        root.setCenter(gridPane);
+
+
+        Scene scene = new Scene(root);
+        stage.setTitle("15x15 grid");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
